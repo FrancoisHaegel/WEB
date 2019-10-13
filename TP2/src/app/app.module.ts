8a494c02273cd8ed8fd3124776477369a83a5c7a
FrancoisHaegel <haegelfrancois@yahoo.fr>
2019-10-13 18:31:36 +0200

    [ADD](TP2): end of 2nd TP

--- TP2/src/app/app.module.ts
+++ TP2/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { PhoneNumberPipe } from "./user-form/phoneNumber.pipe";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TetiereComponent,
+    UserFormComponent,
+    PhoneNumberPipe,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    PhoneNumberPipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
